8c733a4f9c2c6a3c6e30a9e6bf131a72cf7a9ead
luiz.andrade <luiz.andrade@ibbipartner.org.br>
2025-06-06 18:15:48 -0400

    feat: add font style in some h3 and paragraphs

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -23,19 +23,19 @@ export class NewsComponent {
         image: '../../../assets/Image 1.png',
         title: 'Projeto 1',
         date: '31 de Maio de 2025',
-        description: 'Descrição do Projeto 1. Este projeto visa promover a conservação da Amazônia através de práticas sustentáveis e educação ambiental.'
+        description: 'Descrição da Matéria 1. Este é um exemplo de descrição que pode ser usada para detalhar o conteúdo do projeto ou notícia.',
       },
       {
         image: '../../../assets/Image 2.png',
         title: 'Projeto 2',
         date: '31 de Maio de 2025',
-        description: 'Descrição do Projeto 2. Este projeto visa promover a conservação da Amazônia através de práticas sustentáveis e educação ambiental.'
+        description: 'Descrição da Matéria 2. Este é um exemplo de descrição que pode ser usada para detalhar o conteúdo do projeto ou notícia.',
       },
       {
         image: '../../../assets/Image 3.png',
         title: 'Projeto 3',
         date: '31 de Maio de 2025',
-        description: 'Descrição do Projeto 3. Este projeto visa promover a conservação da Amazônia através de práticas sustentáveis e educação ambiental.'
+        description: 'Descrição da Matéria 3. Este é um exemplo de descrição que pode ser usada para detalhar o conteúdo do projeto ou notícia.',
       }
     ]
 }
