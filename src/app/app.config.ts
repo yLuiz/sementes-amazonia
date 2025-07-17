6dafe50525d44f56c44800499e6c56c606157ab6
venicio.conceicao <venicio.conceicao@ibbi.org.br>
2025-07-17 17:58:13 -0400

    feat: api service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ]
 };
