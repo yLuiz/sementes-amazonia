1efa2781063d3b3083cba051b6c8d9cf8841fd81
luiz.andrade <luiz.andrade@ibbipartner.org.br>
2025-07-25 15:40:53 -0300

    update api url

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   useMockData: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://sementes-amazonia-production.up.railway.app',
   apiVersion: 'v1'
 };
 
